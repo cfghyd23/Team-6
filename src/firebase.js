a266e1a593586d664caa1a5027eb0a28fe557904
Mansvi Bhatia <64683559+anonymousb61@users.noreply.github.com>
2023-06-13 19:23:02 +0530

    Update firebase.js
    removed config keys

--- src/firebase.js
+++ src/firebase.js
@@ -12,13 +12,7 @@ import firebaseApp from "./firebase";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "rakhtconnect.firebaseapp.com",
-  projectId: "rakhtconnect",
-  storageBucket: "rakhtconnect.appspot.com",
-  messagingSenderId: "767427242361",
-  appId: "1:767427242361:web:6209537af0d751a31667a8",
-  measurementId: "G-68N0YKECXS"
+
 };
 
 // Initialize Firebase
